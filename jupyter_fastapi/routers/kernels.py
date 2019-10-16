3f2d01fb7ff4055319e24e22315ca717ae07b269
Zsailer <zachsailer@gmail.com>
2019-10-16 15:28:20 -0700

    minor bugs in kernels api

--- jupyter_fastapi/routers/kernels.py
+++ jupyter_fastapi/routers/kernels.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from types import List
+from typing import List
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-from jupyter_server.utils import maybe_future
+from jupyter_server.utils import maybe_future, url_path_join, url_escape
 
 from starlette.websockets import WebSocket
 from starlette.responses import Response
@@ -16,40 +16,41 @@ class Kernel(BaseModel):
     name: str
     last_activity: datetime
     execution_state: str
-    connections: dict
+    connections: int
 
 
 @router.get('/api/kernels', response_model=List[Kernel])
 async def list_kernels():
     self = router.app
     km = self.kernel_manager
-    kernels = await km.list_kernels()
+    kernels = await maybe_future(km.list_kernels())
     # Validate model
     kernels = [Kernel(**k) for k in kernels]
     return kernels
 
 
 class NewKernelModel(BaseModel):
-    name: str
+    name: str = None
+
 
 @router.post(
     '/api/kernels', 
     status_code=201,
     response_model=Kernel
 )
-async def start_kernel(model: NewKernelModel, response: Response):
+async def create_kernel(response: Response, model: NewKernelModel = None):
     self = router.app
     km = self.kernel_manager
     if model is None:
-        model = {
-            'name': km.default_kernel_name
-        }
+        kernel_name = km.default_kernel_name
+    elif model.name is None:
+        kernel_name = km.default_kernel_name
     else:
-        model.setdefault('name', km.default_kernel_name)
-    kernel_id = await km.start_kernel(kernel_name=model['name'])
-    kernel = await km.kernel_model(kernel_id)
+        kernel_name = model.name
+    kernel_id = await maybe_future(km.start_kernel(kernel_name=kernel_name))
+    kernel = await maybe_future(km.kernel_model(kernel_id))
     location = url_path_join(self.base_url, 'api', 'kernels', url_escape(kernel_id))
-    repsonse.headers['Location'] = location
+    response.headers['Location'] = location
     # Validate model!
     return Kernel(**kernel)
 
@@ -71,7 +72,7 @@ async def get_kernel_data(kernel_id: str):
 async def delete_kernel(kernel_id: str):
     self = router.app
     km = self.kernel_manager
-    await km.shutdown_kernel(kernel_id)
+    await maybe_future(km.shutdown_kernel(kernel_id))
 
 
 @router.post(
@@ -94,14 +95,14 @@ async def restart_kernel(kernel_id: str):
     km = self.kernel_manager    
 
     try:
-        await km.restart_kernel(kernel_id)
+        await maybe_future(km.restart_kernel(kernel_id))
     except Exception as e:
         self.log.error("Exception restarting kernel", exc_info=True)
         raise e
     else:
-        model = await km.kernel_model(kernel_id)
+        model = await maybe_future(km.kernel_model(kernel_id))
         return Kernel(**model)
-        
+
 
 # @router.websocket('/api/kernels/{kernel_id}/channels')
 # async def websocket_endpoint(websocket: Websocket):
